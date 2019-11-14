733e907102159a5f606ec14edbf4d1835705b449
Matteo Bucalossi <mbucalossi50@gwu.edu>
2019-11-14 06:13:39 -0500

    Update preprocessing.py
    modified my first script with your edits and removed columns as agreed in class

--- Code/preprocessing.py
+++ Code/preprocessing.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 from pandas.api.types import is_numeric_dtype
 import numpy as np
@@ -6,51 +7,56 @@ import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 
 # read dataset as dataframe
 data = pd.read_csv('real_estate_db.csv', encoding='latin-1')
+
+#Recatogorize the data
+def typeconv(colname):
+    for i in range(0,len(colname),1):
+        data[colname[i]] = data[colname[i]].astype('object')
+
+
+colname = ('UID','SUMLEVEL','COUNTYID','STATEID','zip_code','area_code')
+typeconv(colname)
 
 # shape of dataset
 print("Dataset No. of Rows: ", data.shape[0])
 print("Dataset No. of Columns: ", data.shape[1])
 
-# first dataset observations
-print("Dataset first few rows:\n ")
-print(data.head(5))
-
 # structure of the dataset
 print("Dataset info:\n ")
 print(data.info())
 
+#Drop Duplicates
+data = data.drop_duplicates()
+
 # print null values per column
-nulls = data.isnull().sum()
+nulls = data.isna().sum()
+print("Null values for each column:\n")
 print(nulls)
 
 # drop null column
 data = data.drop('BLOCKID', axis=1)
-print("Dataset Shape: ", data.shape)
 
-# imputation
-def impute(df):
-    for column in df.columns:
-            if column.endswith('median'):
-                imputer = SimpleImputer('median')
-                imputer = imputer.fit(df[:, column])
-                df[:, column] = imputer.transform(df[:, column])
-            else:
-                imputer = SimpleImputer('mean')
-                imputer = imputer.fit(df[:, column])
-                df[:, column] = imputer.transform(df[:, column])
-    return df
+# drop unnecessary columns
+for col in data.columns:
+    if col.endswith(('median', 'stdev', 'samples', 'weight', 'cdf')) | col.startswith('rent_gt'):
+        data = data.drop(col, axis=1)
+print("Dataset Shape after dropping columns:\n", data.shape)
 
-data = impute(data)
+#replacing all the missing values with mean of the column
+data=data.fillna(data.mean()[:])
 
-# delete if imputation works
-data.dropna(inplace=True)
-data.shape[0]
+# print null values per column- to check if null values have been elimnated
+nulls = data.isna().sum()
+print("Null values for each column:\n")
+print(nulls)
 
 
-# outliers
 
 def remove_outliers(df):
     for name in df.columns:
@@ -63,11 +69,15 @@ def remove_outliers(df):
             df = df[(df[name] > lower_bound) & (df[name] < upper_bound)]
     return df
 
-# remove outliers from pop column (before it's geographic data)
-data = remove_outliers(data.loc['pop':])
-
-# these are checks for me if function worked - delete once we are done
-print(sum(data['pop']> 25000))
+# remove outliers - merge outlier table back to
+data1 = remove_outliers(data.loc[:, "ALand":])
+print(data1)
+mergecol = []
+for col in data1.columns:
+    mergecol.append(col)
+data = data.merge(data1, on=mergecol)
+print(data)
+print(data.loc[:, "pop":].head())
 
 for name in data.columns:
     if is_numeric_dtype(data[name]):
